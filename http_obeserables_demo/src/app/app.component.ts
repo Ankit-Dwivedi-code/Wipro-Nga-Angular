ba8d530529ac0568c960439bf835afd8c1a7c63c
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-08-13 12:56:26 +0530

    Implement user service and interceptor; update app component for user list and form handling; enhance styles for user display

--- http_obeserables_demo/src/app/app.component.ts
+++ http_obeserables_demo/src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-import { PostsComponent } from "./posts/posts.component";
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+// import { PostsComponent } from './posts/posts.component';
+import { UserService } from './Services/user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [ PostsComponent],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
+
+
+
+
 export class AppComponent {
   title = 'http_obeserables_demo';
+
+   users: any[] = [];
+
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(data => this.users = data);
+  }
+
+  onSubmit() {
+    console.log('Form submitted:', this.userForm.value);
+    const newUser = this.userForm.value; // Get the form values
+    this.users.push(newUser);  // Add new user to the list
+    this.userForm.reset();  // This resets the form
+    // this.userForm.setValue({ name: '', email: '' }); // Reset form fields
+
+  }
+
+  
+
 }
--- http_obeserables_demo/src/app/app.config.ts
+++ http_obeserables_demo/src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+import { authInterceptor } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    ReactiveFormsModule
+  ]
 };
